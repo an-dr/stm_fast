feed73c99f0d5453cc375af055b868a0f6897c4f
Andrey Gramakov <mail@agramakov.me>
2018-12-03 19:29:51 +0300

    dicsol152
    added cpp macro

--- sf_disco_l152c.h
+++ sf_disco_l152c.h
@@ -7,18 +7,27 @@
 
 #ifndef SF_DISCO_L152C_H_
 #define SF_DISCO_L152C_H_
-
+/*                   */
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+/*                   */
 #include "main.h"
 #include <stdbool.h>
 
 #define GPIO_PIN_BLUE GPIO_PIN_6
 #define GPIO_PIN_GREEN GPIO_PIN_7
 
-
 void SF_Disco_InitLeds();
 void Green(bool state);
 void GreenToggle();
 void Blue(bool state);
 void BlueToggle();
+/*                   */
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
 
 #endif /* SF_DISCO_L152C_H_ */
